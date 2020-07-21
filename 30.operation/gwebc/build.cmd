05ef1cfec37556103f660058efc3236b07e27b21
Matsuo Naoyasu <naoyasumatsuo@gmail.com>
2020-07-22 03:32:05 +0900

    hook

--- 30.operation/gwebc/build.cmd
+++ 30.operation/gwebc/build.cmd
@@ -1,8 +1,14 @@
 :go get github.com/akavel/rsrc
 :cd %GOPATH%
 
+echo %1 %2 
+
 go build -o gwebc-cui.exe
 
 :rsrc -manifest gwebc.exe.manifest -ico gwebc.ico -o gwebc.syso
 rsrc -manifest gwebc.exe.manifest -o gwebc.syso
 go build -ldflags="-H windowsgui"
+if exist dist\gwebc_windows_amd64 (
+  copy /y gwebc-cui.exe  dist\gwebc_windows_amd64
+  copy /y gwebc.exe dist\gwebc_windows_amd64
+)
