35cfb0e2e21f05fefd3e36ee0199b1da4876f28a
Matsuo Naoyasu <naoyasumatsuo@gmail.com>
2022-07-06 09:18:56 +0900

    CHG: sample

--- 40.develop/dev-playwright/sample.spec.ts
+++ 40.develop/dev-playwright/sample.spec.ts
@@ -10,5 +10,6 @@ test('test', async ({ page }) => {
   await page.locator('[aria-label="検索"]').fill('KDDI t');
   // Click text=kddi 通信障害 >> nth=0
   await page.locator('text=kddi 通信障害').first().click();
-  await expect(page).toHaveURL('https://www.google.com/search?q=kddi+%E9%80%9A%E4%BF%A1%E9%9A%9C%E5%AE%B3&source=hp&ei=AMzEYrzABoKl-Qbb6aHoAQ&iflsig=AJiK0e8AAAAAYsTaEFh19DSEYCBPHYk20cqkZWfWJ9JR&oq=KDDI+t&gs_lcp=Cgdnd3Mtd2l6EAEYADINCAAQgAQQsQMQgwEQBDIGCAAQBBADMg0IABCABBCxAxCDARAEMg0IABCABBCxAxCDARAEMgYIABAEEAMyBggAEAQQAzIGCAAQBBADMgYIABAEEANQvjVYvjVgg2JoAXAAeACAAVGIAVGSAQExmAEAoAEBsAEA&sclient=gws-wiz');
+  //await expect(page).toHaveURL('https://www.google.com/search?q=kddi+%E9%80%9A%E4%BF%A1%E9%9A%9C%E5%AE%B3&source=hp&ei=AMzEYrzABoKl-Qbb6aHoAQ&iflsig=AJiK0e8AAAAAYsTaEFh19DSEYCBPHYk20cqkZWfWJ9JR&oq=KDDI+t&gs_lcp=Cgdnd3Mtd2l6EAEYADINCAAQgAQQsQMQgwEQBDIGCAAQBBADMg0IABCABBCxAxCDARAEMg0IABCABBCxAxCDARAEMgYIABAEEAMyBggAEAQQAzIGCAAQBBADMgYIABAEEANQvjVYvjVgg2JoAXAAeACAAVGIAVGSAQExmAEAoAEBsAEA&sclient=gws-wiz');
+  await page.screenshot({ path: 'shotaniNews.png'});
 });
